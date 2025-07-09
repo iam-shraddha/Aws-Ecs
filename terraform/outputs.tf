e27ea77b51b8148810bdc4a5ee0916b03e6ed72f
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-09 15:09:09 +0530

    Update outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.app_repo.repository_url
+}
+
+output "ecs_cluster_name" {
+  description = "ECS cluster name"
+  value       = aws_ecs_cluster.cluster.name
+}
+
+output "ecs_service_name" {
+  description = "ECS service name"
+  value       = aws_ecs_service.app.name
+}
+
+output "alb_dns_name" {
+  description = "Public DNS of the Application Load Balancer"
+  value       = aws_lb.app_alb.dns_name
+}
