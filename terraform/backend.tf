4582642b68f019a809d03842711c7426cbe1075c
shraddha <162278418+iam-shraddha@users.noreply.github.com>
2025-07-10 14:53:17 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-infra-state-ecs-app"
-    key            = "ecs-infra-v2/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
